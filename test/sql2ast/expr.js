florajs/sql-parser

commit 7add00c603be2fe36c5b08c1aeb6bf10aa6301f3

Add basic support for intervals

sql.pegjs
@@ -16,6 +16,7 @@
     'CURRENT_TIMESTAMP': true,
     'CURRENT_USER': true,
 
+    'DAY': true,
     'DELETE': true,
     'DESC': true,
     'DISTINCT': true,
@@ -33,12 +34,14 @@
     'GROUP': true,
 
     'HAVING': true,
+    'HOUR': true,
 
     'IN': true,
     'INDEX': true,
     'INNER': true,
     'INSERT': true,
     'INTO': true,
+    'INTERVAL': true,
     'IS': true,
 
     'JOIN': true,
@@ -48,6 +51,10 @@
     'LIKE': true,
     'LIMIT': true,
 
+    'MICROSECOND': true,
+    'MINUTE': true,
+    'MONTH': true,
+
     'NOT': true,
     'NULL': true,
 
@@ -56,10 +63,13 @@
     'ORDER': true,
     'OUTER': true,
 
+    'QUARTER': true,
+
     'RECURSIVE': true,
     'REPLACE': true,
     'RIGHT': true,
 
+    'SECOND': true,
     'SELECT': true,
     'SESSION_USER': true,
     'SET': true,
@@ -80,8 +90,11 @@
     'VALUES': true,
 
     'WITH': true,
+    'WEEK': true,
     'WHEN': true,
-    'WHERE': true
+    'WHERE': true,
+
+    'YEAR': true
   };
 
   function createUnaryExpr(op, e) {
@@ -625,6 +638,7 @@ primary
         return list;
     }
   / var_decl
+  / interval_expr
 
 column_ref
   = tbl:ident __ DOT __ col:column {
@@ -785,6 +799,23 @@ cast_expr
       }
     };
   }
+
+interval_expr
+  = KW_INTERVAL __  sign:("+" / "-")? __ "'"? __ value:int "'"? __ qualifier:interval_unit {
+    return {
+      type: 'interval',
+      sign: sign,
+      value: value,
+      qualifier: qualifier
+    };
+  }
+
+interval_unit
+  = KW_MINUTE
+  / KW_HOUR
+  / KW_DAY
+  / KW_MONTH
+  / KW_YEAR
 
 signedness
   = KW_SIGNED
@@ -974,6 +1005,13 @@ KW_DATE     = "DATE"i     !ident_start { return 'DATE'; }
 KW_TIME     = "TIME"i     !ident_start { return 'TIME'; }
 KW_TIMESTAMP= "TIMESTAMP"i!ident_start { return 'TIMESTAMP'; }
 KW_USER     = "USER"i     !ident_start { return 'USER'; }
+
+KW_INTERVAL     = "INTERVAL"i !ident_start { return 'INTERVAL'; }
+KW_MINUTE       = "MINUTE"i !ident_start { return 'MINUTE'; }
+KW_HOUR         = "HOUR"i !ident_start { return 'HOUR'; }
+KW_DAY          = "DAY"i !ident_start { return 'DAY'; }
+KW_MONTH        = "MONTH"i !ident_start { return 'MONTH'; }
+KW_YEAR         = "YEAR"i !ident_start { return 'YEAR'; }
 
 KW_CURRENT_DATE     = "CURRENT_DATE"i !ident_start { return 'CURRENT_DATE'; }
 KW_CURRENT_TIME     = "CURRENT_TIME"i !ident_start { return 'CURRENT_TIME'; }

lib/sql.js
@@ -44,6 +44,10 @@ function literalToSQL(literal) {
     else if (type === 'star') value = '*';
     else if (['time', 'date', 'timestamp'].includes(type)) value = `${type.toUpperCase()} '${value}'`;
     else if (type === 'param') value = ':' + value;
+    else if (type === 'interval') {
+        const sign = literal.sign ? literal.sign + ' ' : '';
+        value = `INTERVAL ${sign}'${escape(value)}' ${literal.qualifier}`;
+    }
 
     return !literal.parentheses ? value : '(' + value + ')';
 }

test/ast.spec.js
@@ -190,6 +190,23 @@ describe('AST', () => {
                 sql = `SELECT "id" FROM DUAL`;
                 expect(getParsedSql(sql)).to.equal(sql);
             });
+
+            describe('interval', () => {
+                it('should parse simple INTERVAL', () => {
+                    sql = 'SELECT NOW() + INTERVAL 1 DAY FROM DUAL';
+                    expect(getParsedSql(sql)).to.equal(`SELECT NOW() + INTERVAL '1' DAY FROM DUAL`);
+                });
+
+                it('should parse string INTERVAL', () => {
+                    sql = `SELECT NOW() + INTERVAL '1' DAY FROM DUAL`;
+                    expect(getParsedSql(sql)).to.equal(sql);
+                });
+
+                it('should parse signed INTERVAL', () => {
+                    sql = `SELECT NOW() + INTERVAL - '1' DAY FROM DUAL`;
+                    expect(getParsedSql(sql)).to.equal(sql);
+                });
+            });
         });
 
         describe('joins', () => {

test/select.spec.js
@@ -125,6 +125,120 @@ describe('select', () => {
                 }
             ]);
         });
+
+        describe('interval', () => {
+            describe('qualifiers', () => {
+                [
+                    'MINUTE',
+                    'HOUR',
+                    'DAY',
+                    'MONTH',
+                    'YEAR'
+                ].forEach(qualifier => {
+                    it(`should support ${qualifier}`, () => {
+                        ast = parser.parse(`SELECT CURRENT_DATE + INTERVAL 10 ${qualifier} FROM dual`);
+
+                        expect(ast.columns).to.deep.contain({
+                            expr: {
+                                type: 'binary_expr',
+                                operator: '+',
+                                left: {
+                                    type: 'function',
+                                    name: 'CURRENT_DATE',
+                                    args: { type: 'expr_list', value: [] }
+                                },
+                                right: {
+                                    type: 'interval',
+                                    sign: null,
+                                    value: '10',
+                                    qualifier: qualifier
+                                }
+                            },
+                            as: null
+                        });
+                    });
+                });
+            });
+
+            [
+                ['should support intervals with explicit plus sign in interval qualifier', '+10', '+', '10'],
+                ['should support negative interval qualifier', '-10', '-', '10']
+            ].forEach(([description, interval, sign, expectedResult]) => {
+                it(description, () => {
+                    ast = parser.parse(`SELECT CURRENT_DATE + INTERVAL ${interval} DAY FROM dual`);
+
+                    expect(ast.columns).to.deep.contain({
+                        expr: {
+                            type: 'binary_expr',
+                            operator: '+',
+                            left: {
+                                type: 'function',
+                                name: 'CURRENT_DATE',
+                                args: { type: 'expr_list', value: [] }
+                            },
+                            right: {
+                                type: 'interval',
+                                sign: sign,
+                                value: expectedResult,
+                                qualifier: 'DAY'
+                            }
+                        },
+                        as: null
+                    });
+                });
+            });
+
+            it('should support intervals as strings', () => {
+                ast = parser.parse(`SELECT CURRENT_DATE + INTERVAL '10' DAY FROM dual`);
+
+                expect(ast.columns).to.deep.contain({
+                    expr: {
+                        type: 'binary_expr',
+                        operator: '+',
+                        left: {
+                            type: 'function',
+                            name: 'CURRENT_DATE',
+                            args: { type: 'expr_list', value: [] }
+                        },
+                        right: {
+                            type: 'interval',
+                            sign: null,
+                            value: '10',
+                            qualifier: 'DAY'
+                        }
+                    },
+                    as: null
+                });
+            });
+
+            [
+                ['should support positive sign', '+', '-10'],
+                ['should support negative sign', '-', '-10']
+            ].forEach(([description, sign, expectedResult]) => {
+                it(description, () => {
+                    ast = parser.parse(`SELECT CURRENT_DATE + INTERVAL ${sign} '-10' DAY FROM dual`);
+
+                    expect(ast.columns).to.deep.contain({
+                        expr: {
+                            type: 'binary_expr',
+                            operator: '+',
+                            left: {
+                                type: 'function',
+                                name: 'CURRENT_DATE',
+                                args: {type: 'expr_list', value: []}
+                            },
+                            right: {
+                                type: 'interval',
+                                sign: sign,
+                                value: expectedResult,
+                                qualifier: 'DAY'
+                            }
+                        },
+                        as: null
+                    });
+                });
+            });
+        });
     });
 
     describe('from clause', () => {

commit e5dac50476f29e8db372094ee92a45b00c8af119

Quick fix for date functions
The bug was introduced by INTERVAL support in 7add00c.

sql.pegjs
@@ -709,6 +709,9 @@ func_call
   = name:ident __ LPAREN __ l:expr_list? __ RPAREN {
       return { type: 'function', name, args: l ? l : { type: 'expr_list', value: [] } };
     }
+  / name:(KW_YEAR / KW_MONTH / KW_DAY / KW_HOUR / KW_MINUTE) __ LPAREN __ l:expr_list? __ RPAREN {
+      return { type: 'function', name, args: l ? l : { type: 'expr_list', value: [] } };
+    }
   / name:scalar_func {
       return { type: 'function', name, args: { type: 'expr_list', value: [] } };
     }

test/sql2ast/expr.js
@@ -3,7 +3,6 @@
 const { expect } = require('chai');
 const { Parser } = require('../../');
 
-
 describe('expressions', () => {
     const parser = new Parser();
 
@@ -17,13 +16,39 @@ describe('expressions', () => {
                         type: 'function',
                         name: 'fun',
                         args: {
-                            type  : 'expr_list',
-                            value : [ { type: 'column_ref', table: null, column: 'd' } ]
+                            type: 'expr_list',
+                            value: [{ type: 'column_ref', table: null, column: 'd' }]
                         }
                     },
                     as: null
                 }
             ]);
+        });
+
+        ['year', 'month', 'day', 'hour', 'minute'].forEach((fn) => {
+            it(`should parse ${fn} date function expression`, () => {
+                const ast = parser.parse(`SELECT ${fn}(NOW()) FROM dual`);
+
+                expect(ast.columns).to.eql([
+                    {
+                        expr: {
+                            type: 'function',
+                            name: fn.toUpperCase(),
+                            args: {
+                                type: 'expr_list',
+                                value: [
+                                    {
+                                        type: 'function',
+                                        name: 'NOW',
+                                        args: { type: 'expr_list', value: [] }
+                                    }
+                                ]
+                            }
+                        },
+                        as: null
+                    }
+                ]);
+            });
         });
 
         [