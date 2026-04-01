9968b70a949c8cd545c32b78205d725cd0f54e0b
Marco Baumgartl <marco.baumgartl@stock3.com>
2026-04-01 12:46:51 +0200

    chore: Update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,28 +2,24 @@ const js = require('@eslint/js');
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = [
+    {
+        files: ['index.js', 'lib/*.js', 'test/**/*.spec.js', 'eslint.config.js']
+    },
+    {
+        ignores: ['node_modules/', 'dist/']
+    },
     js.configs.recommended,
     eslintPluginPrettierRecommended,
     {
         languageOptions: {
             sourceType: 'commonjs',
-            parserOptions: {
-                ecmaVersion: 2018
-            }
-        }
-    },
-    {
+            ecmaVersion: 2018
+        },
         rules: {
             'no-console': 'warn',
             'no-unused-vars': 'off',
             'require-atomic-updates': 'off',
             'no-prototype-builtins': 'off'
         }
-    },
-    {
-        files: ['index.js', 'lib/*.js', 'test/**/*.spec.js']
-    },
-    {
-        ignores: ['node_modules/', 'dist/']
     }
 ];
